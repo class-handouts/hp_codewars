51b226c44270b7cd46c20599d2591183feb922b1
Clayton Dagler at Franklin HS <cdagler@e468098261ucccp.egusd.net>
2020-03-02 12:49:46 -0800

    Updated PrimeNumbersRunner.java

--- 05_starter_code/PrimeNumbersRunner.java
+++ 05_starter_code/PrimeNumbersRunner.java
@@ -9,7 +9,8 @@ import java.util.ArrayList;
 
 public class PrimeNumbersRunner
 {
-   public static void main(String[] args) {
+   public static void main(String[] args) 
+   {
       PrimeNumbers primeNumbers = new PrimeNumbers();
       int n = 2019;
 
@@ -28,53 +29,63 @@ public class PrimeNumbersRunner
    }
 }
 
-class PrimeNumbers {
-   private ArrayList<Integer> primeNums;
+class PrimeNumbers 
+{
+    private ArrayList<Integer> primeNums;
 
-   public PrimeNumbers() {
-      primeNums = new ArrayList<Integer>();
-   }
+    public PrimeNumbers() 
+    {
+        primeNums = new ArrayList<Integer>();
+    }
 
    /** This list the first n prime number.
     * 
     * @param n The number of primes to return
     * @return The frist n primes stored in an ArrayList.
     */
-   ArrayList getList(int n) {
-      int currentNumPrimes = 1, val = 2;
-      boolean isPrime;
+    ArrayList getList(int n) 
+    {
+        int currentNumPrimes = 1, val = 2;
+        boolean isPrime;
 
-      while (currentNumPrimes <= n) {
-         if (primeNums.size() == 0) {
-            primeNums.add(val);
-            currentNumPrimes++;
-         } else {
-            val++;
-            isPrime = true;
-            for (int primeNum : primeNums) {
-               if (val % primeNum == 0) {
-                  isPrime = false;
-                  break;
-               }
+        while (currentNumPrimes <= n) 
+        {
+            if (primeNums.size() == 0) 
+            {
+                primeNums.add(val);
+                currentNumPrimes++;
+            } 
+            else 
+            {
+                val++;
+                isPrime = true;
+                for (int primeNum : primeNums) 
+                {
+                    if (val % primeNum == 0) 
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) 
+                {
+                    primeNums.add(val);
+                    currentNumPrimes++;
+                }
             }
-            if (isPrime) {
-               primeNums.add(val);
-               currentNumPrimes++;
-            }
-         }
-      }
-      return primeNums;
-   }
+        }
+        return primeNums;
+    }
 
-   public boolean isPrime(int n) 
-   {
-      int sqrtN = (int)Math.sqrt(n)+1;
+    public boolean isPrime(int n) 
+    {
+        int sqrtN = (int)Math.sqrt(n)+1;
 
-      for(int i=2; i<sqrtN; i++)
-      {
-         if (n % i == 0)
+        for(int i=2; i<sqrtN; i++)
+        {
+            if (n % i == 0)
             return false;
-      }
-      return true;
-   }
+        }
+        return true;
+    }
 }
